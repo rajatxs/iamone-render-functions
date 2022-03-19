afc966abcef2781691c554c741a8f6f5367befc5
Rajat Sharma <rxx175@gmail.com>
2022-03-19 21:55:42 +0530

    Update CORS Origin

--- middlewares/common.ts
+++ middlewares/common.ts
@@ -1,7 +1,7 @@
 import { AppMiddleware } from '@rxpm/vsfm'
 
 export const corsMiddleware: AppMiddleware = (request, response, next) => {
-   response.setHeader('Access-Control-Allow-Origin', '*')
+   response.setHeader('Access-Control-Allow-Origin', 'https://app.iamone.link')
    response.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
    response.setHeader('Access-Control-Allow-Headers', 'X-Client-Id, Content-Type')
 
